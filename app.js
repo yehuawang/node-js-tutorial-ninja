a789a717acec85497fa6d8557421fc88ce2f05b9
cangyao <yehuaywang@gmail.com>
2024-09-04 20:30:59 -0400

    post, create, and delete blogs allowed. Wanted to fix: preserve user entered format of the blog, including change line ...

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const morgan = require('morgan'); //morgan is a middleware that logs information about the request to the console
 const mongoose = require('mongoose'); //mongoose is a library that makes it easier to work with MongoDB
 const Blog = require('./models/blog');
+const { result } = require('lodash');
 
 // express app
 const app = express();
@@ -22,33 +23,21 @@ app.set('view engine', 'ejs');
 
 // middleware & static files
 app.use(express.static('public')); //this will serve static files from the public folder
+app.use(express.urlencoded({ extended: true }));
 app.use(morgan('dev'));
 
 
 // routes
 app.get('/', (req, res) => {
-    // const blogs = [
-    //     { 
-    //         title: 'The Science of Black Holes: Exploring the Unknown', 
-    //         snippet: 'Black holes are one of the most mysterious and fascinating objects in the universe. They are regions of spacetime where gravity is so strong that nothing, not even light, can escape from them.' 
-    //     },
-    //     { 
-    //         title: 'CRISPR: The Future of Genetic Engineering', 
-    //         snippet: 'CRISPR is a revolutionary technology that allows scientists to edit genes with unprecedented precision, speed, and flexibility. It has the potential to transform the fields of medicine, agriculture, and biotechnology.' 
-    //     },
-    //     { 
-    //         title: 'Quantum Computing: The Next Frontier of Information Technology', 
-    //         snippet: 'Quantum computing is a new paradigm of computation that harnesses the laws of quantum mechanics to process information in ways that are impossible with classical computers. It has the potential to revolutionize fields such as cryptography, drug discovery, and artificial intelligence.' 
-    //     }
-    // ];
-
-    // res.render('index', { title: 'Home', blogs: blogs }); //render the index.ejs file
-
     res.redirect('/blogs');
 });
 
 app.get('/about', (req, res) => {
     res.render('about', { title: 'About' });
+});
+
+app.get('/blogs/create', (req, res) => {
+    res.render('create', { title: 'Create New Blog' });
 });
 
 
@@ -64,10 +53,49 @@ app.get('/blogs', (req, res) => {
         .catch((err) => {
             console.log(err);
         });
-})
-app.get('/blogs/create', (req, res) => {
-    res.render('create', { title: 'Create New Blog' });
-})
+});
+
+// POST handler
+app.post('/blogs', (req, res) => {  
+    const blog = new Blog(req.body);
+    blog.save()
+        .then((result) => {
+            res.redirect('/blogs');
+        })
+        .catch((err) => {
+            console.log(err);
+        })
+});
+
+// GET specific blog
+app.get('/blogs/:id', (req, res) => {
+    const id = req.params.id;
+    Blog.findById(id)
+        .then((result) => {
+            res.render('details', { blog: result, title: 'Blog Details' });
+        }) 
+        .catch((err) => {
+            console.log(err);
+        });
+});
+
+// DELETE request handler
+
+app.delete('/blogs/:id', (req, res) => {
+    const id = req.params.id;
+
+    Blog.findByIdAndDelete(id)
+        .then(result => {
+            res.json({ redirect: '/blogs' });
+        })
+        .catch((err) => {
+            console.log(err);
+        });
+});
+
+
+
+
 
 
 // 404 page
